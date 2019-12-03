d190582d4709dc4df75b4fa8d383cf3564be8462
bob hall <myk_quayce@hotmail.com>
2019-12-03 23:10:15 +0000

    day 3 part 2

--- AdventOfCode2019.Day03/UnitTest1.cs
+++ AdventOfCode2019.Day03/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace AdventOfCode2019.Day03
         [InlineData("R8,U5,L5,D3", "U7,R6,D4,L4", 6)]
         [InlineData("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 159)]
         [InlineData("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 135)]
-        public void Test1(string path1Csv, string path2Csv, int expected)
+        public void WalkTests(string path1Csv, string path2Csv, int expected)
         {
             var points = new List<Point>();
 
@@ -61,10 +61,70 @@ namespace AdventOfCode2019.Day03
             Assert.Equal(expected, closest);
         }
 
-        private static IReadOnlyCollection<Point> Walk(IEnumerable<string> path)
+        [Theory]
+        [InlineData("R8,U5,L5,D3", "U7,R6,D4,L4", 30)]
+        [InlineData("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 610)]
+        [InlineData("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 410)]
+        public void ShortestIntersectionTests(string path1Csv, string path2Csv, int expected)
+        {
+            var path1 = Walk(path1Csv.Split(','));
+            var path2 = Walk(path2Csv.Split(','));
+
+            var actual = ShortestIntersection(path1, path2);
+
+            Assert.Equal(expected, actual);
+        }
+
+        private static int ShortestIntersection(IEnumerable<Point> first, IEnumerable<Point> second)
+        {
+            var index = 0;
+            var shortest = int.MaxValue;
+            var list = first.ToList();
+
+            foreach (var point in second)
+            {
+                if (!point.IsEmpty)
+                {
+                    for (var a = 0; a < list.Count; a++)
+                    {
+                        if (shortest < index + a) continue;
+                        if (point != list[a]) continue;
+
+                        shortest = index + a;
+                    }
+                }
+
+                index++;
+            }
+
+            return shortest;
+        }
+
+        [Theory]
+        [InlineData("input.txt", 112_316)]
+        public async Task SolvePart2(string fileName, int expected)
+        {
+            var lines = Helpers.ReadLinesAsync(fileName);
+            var csves = new List<IList<string>>();
+
+            await foreach(var line in lines)
+            {
+                var values = line.Split(',');
+                csves.Add(values);
+            }
+
+            var first = Walk(csves[0]);
+            var second = Walk(csves[1]);
+
+            var actual = ShortestIntersection(first, second);
+
+            Assert.Equal(expected, actual);
+        }
+
+        private static IEnumerable<Point> Walk(IEnumerable<string> path)
         {
             var curr = new Point(x: 0, y: 0);
-            var points = new List<Point> { curr, };
+            yield return curr;
 
             foreach (var line in path)
             {
@@ -81,11 +141,9 @@ namespace AdventOfCode2019.Day03
                         _ => throw new ArgumentOutOfRangeException(nameof(line), line),
                     };
 
-                    points.Add(curr);
+                    yield return curr;
                 }
             }
-
-            return points.Distinct().ToList();
         }
     }
 }
